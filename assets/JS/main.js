be8a7267e99b4c4a2f0cc042dd7d0f549cfa0140
Sama Hassan <samahassanmohamed12@gmail.com>
2026-04-13 14:01:59 +0200

    dark-theme

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -9,11 +9,26 @@ window.onscroll = function () {
 
 const skillsObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        if (entry.isIntersecting) {   entry.target.classList.add('active');}
+        if (entry.isIntersecting) { entry.target.classList.add('active'); }
         else { entry.target.classList.remove('active'); }
     });
-}, 
-{  threshold: 0.2 });
+},
+    { threshold: 0.2 });
 document.querySelectorAll('.skills').forEach(skill => {
     skillsObserver.observe(skill);
+});
+
+const mode_btn = document.getElementById("mode-btn");
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+}
+mode_btn.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 });
